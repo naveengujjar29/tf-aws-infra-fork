f0fda0e068cf9fc935d37edd76737e0ae0c63c6e
Naveen Kumar <naveengujjar29@gmail.com>
2024-10-08 09:50:56 +0530

    changes to handle tf deployment

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "aws" {
 }
 
 module "vpc" {
-  source       = "../../modules/vpc"
+  source       = "./modules/vpc"
   vpc_name     = var.vpc_name
   cidr_block   = var.vpc_cidr
 }
 
 module "subnets" {
-  source             = "../../modules/subnets"
+  source             = "./modules/subnets"
   vpc_id             = module.vpc.vpc_id
   public_subnets     = var.public_subnets
   private_subnets    = var.private_subnets
@@ -18,12 +18,12 @@ module "subnets" {
 }
 
 module "internet_gateway" {
-  source = "../../modules/internet-gateway"
+  source = "./modules/internet-gateway"
   vpc_id = module.vpc.vpc_id
 }
 
 module "route_tables" {
-  source                 = "../../modules/route-tables"
+  source                 = "./modules/route-tables"
   vpc_id                 = module.vpc.vpc_id
   public_subnets         = module.subnets.public_subnet_ids
   private_subnets        = module.subnets.private_subnet_ids
