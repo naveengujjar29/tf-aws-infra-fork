97ee6d0cd49badb247f09a08b313db327fc3828f
Naveen Kumar <naveengujjar29@gmail.com>
2024-10-08 17:41:39 +0530

    changes to handle tf deployment

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ resource "random_string" "suffix" {
 }
 
 module "vpc" {
-  source       = "./modules/vpc"
-  vpc_name     =  var.vpc_name != "" ? var.vpc_name : "assignment-vpc-${random_string.suffix.result}"
-  cidr_block   = var.vpc_cidr
+  source     = "./modules/vpc"
+  vpc_name   = var.vpc_name != "" ? var.vpc_name : "assignment-vpc-${random_string.suffix.result}"
+  cidr_block = var.vpc_cidr
 }
 
 module "subnets" {
@@ -21,15 +21,15 @@ module "subnets" {
 }
 
 module "internet_gateway" {
-  source = "./modules/internet-gateway"
-  vpc_id = module.vpc.vpc_id
+  source       = "./modules/internet-gateway"
+  vpc_id       = module.vpc.vpc_id
   gateway_name = var.gateway_name != "" ? var.gateway_name : "assignment-igw-${random_string.suffix.result}"
 }
 
 module "route_tables" {
-  source                 = "./modules/route-tables"
-  vpc_id                 = module.vpc.vpc_id
-  public_subnets         = module.subnets.public_subnet_ids
-  private_subnets        = module.subnets.private_subnet_ids
-  internet_gateway_id    = module.internet_gateway.igw_id
+  source              = "./modules/route-tables"
+  vpc_id              = module.vpc.vpc_id
+  public_subnets      = module.subnets.public_subnet_ids
+  private_subnets     = module.subnets.private_subnet_ids
+  internet_gateway_id = module.internet_gateway.igw_id
 }
--- variables.tf
+++ variables.tf
@@ -35,6 +35,5 @@ variable "private_subnets" {
 variable "availability_zones" {
   description = "List of Availability zones"
   type        = list(string)
-  default     = ["us-east-1a","us-east-1b","us-east-1c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
-
